8544372d38fcba7aff7992afe70b274c66aeaad8
Malte Polzin <malte@polz.in>
2023-10-21 15:32:59 +0200

    Initial Commit

---
+++ Spandex/SpandexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpandexApp.swift
+//  Spandex
+//
+//  Created by Malte Polzin on 21.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SpandexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

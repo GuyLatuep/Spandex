ce1dffdc7547ecca56d56c043fda0e533e8e8d87
Malte Polzin <malte@polz.in>
2023-10-22 19:06:47 +0200

    adding matching function and tests for matching function

--- Spandex/ContentView.swift
+++ Spandex/ContentView.swift
@@ -4,48 +4,104 @@
 //
 //  Created by Malte Polzin on 21.10.23.
 //
+/**
+ Todo
+ 1. Text Tracking - done
+ 2. Snippet Matching
+ 3. Insert Snippet
+ 3. a remove trigger text
+ 3. b insert snippet
+
+ 
+ 
+ */
+
 
 import SwiftUI
 
 struct Snippet {
     let trigger: String
     let content: String
+    /**
+     example xname
+     ##should Match
+     -xname
+     - xname
+     -the xname
+     ##shouldn´t match
+     -xnot
+     -xnam
+     -theuxname
+     */
+    func matches(_ string: String) -> Bool {
+        let hasSuffix = string.hasSuffix(trigger)
+        let isBoundary = (string.dropLast(trigger.count).last ?? " ").isWhitespace
+        return hasSuffix && isBoundary
+    }
 }
-
+extension Snippet {
+    static var examples : [Snippet] = [
+        Snippet(trigger: "xname", content: "Malte Polzin"),
+        Snippet(trigger: "xsite", content: "polz.in"),
+        Snippet(trigger: "xsnippet", content: "StringSnippet(trigger: \"<#trigger#>\", content: \"<#content#>\"),"),
+        ]
+}
 extension NSEvent {
     var isDelete: Bool {
-        keyCode == 53
+        keyCode == 51
     }
 }
 
 class SpandexModel: ObservableObject {
+    @Published var text = ""
+    @Published var snippets = Snippet.examples
     init() {
+        NSEvent.addGlobalMonitorForEvents(matching: [.leftMouseDown, .rightMouseDown, .otherMouseDown]) { event in
+            self.text = ""
+        }
         NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { (event) in
             guard let characters = event.characters else {return}
             print(characters, event.keyCode)
+            if event.isDelete && !self.text.isEmpty {
+                self.text.removeLast()
+            } else if event.keyCode > 100 {
+                self.text = ""
+            } else {
+                self.text += characters
+            }
+            
+            self.matchsnaippet()
         }
     }
+    
+    func matchsnaippet() {
+        if let match = snippets.first(where: { $0.matches(self.text) }) {
+            insertSnippet(match)
+        }
+    }
+    
+    func insertSnippet(_ snippet: Snippet) {
+        print("inserting \(snippet)")
+    }
 }
+
 struct ContentView: View {
     @StateObject var model = SpandexModel()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(model.text)")
+            List(model.snippets, id: \.trigger) { snippet in
+                HStack {
+                    Text(snippet.trigger)
+                    Text(snippet.content).lineLimit(1)
+                }.foregroundColor(snippet.matches(model.text) ? Color.red : Color.primary)
+            }
         }
         .padding()
+        .frame(width: 300, height: 300)
     }
 }
 
 #Preview {
     ContentView()
 }
-
-enum Color: CaseIterable {
-    case blue
-    case white
-    case red
-}
-
--- SpandexTests/SpandexTests.swift
+++ SpandexTests/SpandexTests.swift
@@ -10,27 +10,29 @@ import XCTest
 
 final class SpandexTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
+    func testSnippingMatching() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        /**
+         example xname
+         ##should Match
+         -xname
+         - xname
+         -the xname
+         ##shouldn´t match
+         -xnot
+         -xnam
+         -theuxname
+         */
+        let snippet = Snippet(trigger: "xname", content: "Malte Polzin")
+        XCTAssert(snippet.matches("xname"))
+        XCTAssert(snippet.matches(" xname"))
+        XCTAssert(snippet.matches("the xname"))
+        XCTAssert(!snippet.matches("xnot"))
+        XCTAssert(!snippet.matches("xnam"))
+        XCTAssert(!snippet.matches("theuxname"))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
